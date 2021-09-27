b7d538342714a8b18e6589c43e5f19439522465f
ThaFreaky <rollf97@gmail.com>
2021-09-27 11:57:22 +0200

    MainUserRegActivity -> UserRegFragment

--- app/src/main/java/com/example/covidapp/HealthAdmin/AdminMenuFragment.java
+++ app/src/main/java/com/example/covidapp/HealthAdmin/AdminMenuFragment.java
@@ -65,8 +65,9 @@ public class AdminMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_menu, container, false);
+        binding = FragmentAdminMenuBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
     }
 
     @Override
