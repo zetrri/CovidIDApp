d0490b3e7d0bcb8b92896efe0597bb5defc3b21b
leon <zetrri@gmail.com>
2021-10-06 21:45:20 +0200

    fixed admin add times

--- app/src/main/java/com/example/covidapp/MyPage/MyPageFragment.java
+++ app/src/main/java/com/example/covidapp/MyPage/MyPageFragment.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
